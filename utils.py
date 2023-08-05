42e31fff8243f1c02a3188bb71ff79ab6d5b852c
Vijay <s.vijay12062005@gmail.com>
2023-08-05 14:09:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -222,14 +222,14 @@ def get_size(size):
 
 def filtered_file_name(file_name):
     import re
-    # Remove words between square brackets
+    # Remove words between square brackets (including spaces)
     file_name = re.sub(r'\[.*?\]', '', file_name)
     
-    prefixes = ('@', '_', 'www.', 'tv')
+    prefixes = ('@', 'www.', 'tv')
     filtered_words = filter(lambda x: not any(x.startswith(prefix) for prefix in prefixes), file_name.split())
     
-    # Remove words that match the format of a Telegram username
-    filtered_words = [word for word in filtered_words if not re.match(r'@\w+', word)]
+    # Remove words that match the format of a Telegram username (including underscores)
+    filtered_words = [word for word in filtered_words if not re.match(r'@[\w_]+', word)]
     
     return ' '.join(filtered_words)
 
