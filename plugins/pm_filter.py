b2326b9a31e2458c9e235058fca3e4e3b042ff06
Vijay <s.vijay12062005@gmail.com>
2023-08-05 12:51:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -123,7 +123,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('@') and not x.startswith('_') and not x.startswith('www.'), file.file_name.split()))}", callback_data=f'{pre}#{file.file_id}'
+    text=f"[{get_size(file.file_size)}] {' '.join(filtered_file_name(file.file_id).split())}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
